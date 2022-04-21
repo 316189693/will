ef636ff1ebe75264ecc32d66f4d02fd44037abdb
will <will.zheng@unisco.com>
2022-04-21 14:52:44 +0800

    dd

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+let fs = require('fs');
+let https = require("https");
+let options = {
+    key: fs.readFileSync("./enc/privkey.pem"),
+    cert: fs.readFileSync("./enc/cacert.pem")
+};
 
 const app = express();
 app.use(express.static("./dist"))
-app.listen(8000, function(err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('start success')
-    }
-})
+
+let server = https.createServer(options, app);
+server.listen(8000);
