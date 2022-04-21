ec56fbaa60abdd4540bb64867627032abd5bfac8
will <will.zheng@unisco.com>
2022-04-21 15:48:05 +0800

    dd

--- infrastructure/axiosService.js
+++ infrastructure/axiosService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 console.log("axios")
 let instance = axios.create({
-    baseURL: 'https://127.0.0.1:7000',
+    baseURL: 'https://42.192.226.123:7000',
     timeout: 3000,
     headers: {"Content-Type": "application/json; charset=UTF-8"}
 })
