d3efc0ecc11443c39e9d4501916372c33d8ac641
will <will.zheng@unisco.com>
2022-04-21 13:40:37 +0800

    ddd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.static("./dist"))
-app.listen(3000, function(err) {
+app.listen(8000, function(err) {
     if (err) {
         console.log(err)
     } else {
