0e33b4c96e4a07c73c4efb29fa0fd75970a665e4
will <will.zheng@unisco.com>
2022-04-22 09:59:41 +0800

    dd

--- infrastructure/axiosService.js
+++ infrastructure/axiosService.js
@@ -1,12 +1,9 @@
-import axios from 'axios'
+let axios = require('axios');
 let env = process.env.NODE_ENV !== 'production' ? 'dev':'prod';
-let cfg_path = __dirname + "/../../will-config/will-front/"+env+"/config.json";
-console.log(cfg_path);
-let cfg = require(cfg_path);
-
+let  cfg = require("../../will-config/will-front/"+env+"/config.json");
 let instance = axios.create({
     baseURL: cfg.backend_base_url,
     timeout: cfg.timeout,
     headers: {"Content-Type": "application/json; charset=UTF-8"}
 })
-export default instance;
+module.exports = instance;
